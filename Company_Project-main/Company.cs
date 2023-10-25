e133910736bc07f8b6178c0e28c168c16dd1df23
RufatAzizov <146819407+RufatAzizov@users.noreply.github.com>
2023-10-25 19:03:52 +0400

    Add files via upload

---
+++ Company_Project-main/Company.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company_Project
+{
+    public class Company
+    {
+        string _name;
+        public string Name 
+        {
+            get
+            {
+                return this.Name;
+            }
+            set
+            {
+                
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    value = value.Trim();
+                    this._name = value.ToUpper()[0] + value.Substring(1);
+                }
+            }
+        }
+        public Employee[] Employees; //new Employee[0]
+
+        public Company(string name)
+        {
+            Name = name;
+            Employees = new Employee[0];
+        }
+
+        public void  AddUser(Employee employee)
+        {
+            Array.Resize(ref Employees, Employees.Length+1);
+            Employees[Employees.Length-1] = employee;
+            Console.WriteLine("Successfully added!");
+        }
+        public Employee GetUser(string username) 
+        { 
+            foreach (Employee emp in Employees)
+            {
+                if (emp.Username == username) {  return emp; }
+            }
+            Console.WriteLine("Employee with given username not found");
+            return null; 
+        }
+
+        public Employee UpdateUser(string username)
+        {
+            for (int i = 0; i < Employees.Length; i++)
+            {
+                if (Employees[i].Username == username)
+                {
+                    Console.WriteLine($"What do you want to update for {username}?\n");
+                    Console.WriteLine("1. Update Name");
+                    Console.WriteLine("2. Update Surname");
+                    Console.WriteLine("3. Update Age");
+                }
+            }
+
+            Console.WriteLine($"Employee with given username {username} not found");
+            return null;
+        }
+
+
+        public void GetAllUser()
+        {
+            foreach ( Employee users in Employees)
+            {
+                Console.WriteLine(users);
+            }
+
+
+            
+        }
+
+        public void RemoveUser(string username)
+        {
+            int indexToRemove = -1;
+            for (int i = 0; i < Employees.Length; i++)
+            {
+                if (Employees[i].Username == username)
+                {
+                    indexToRemove = i;
+                    break;
+                }
+            }
+
+            if (indexToRemove != -1)
+            {
+                for (int i = indexToRemove; i < Employees.Length - 1; i++)
+                {
+                    Employees[i] = Employees[i + 1];
+                }
+                Array.Resize(ref Employees, Employees.Length - 1);
+                Console.WriteLine($"Employee {username} has been removed successfully.");
+            }
+            else
+            {
+                Console.WriteLine($"Employee with username {username} not found.");
+            }
+        }
+    }
+}
+
---
+++ Company_Project-main/Program.cs
@@ -0,0 +1,72 @@
+﻿using Company_Project;
+
+/*Employee e = new Employee("vugar","sariyev","vugar123");
+
+Company company1 = new Company("Socar kbr");*/
+
+
+
+
+/*while (true) // quit
+{
+    Console.WriteLine("1. Create a company");
+    Console.WriteLine("2. Create an employee");
+    Console.WriteLine("3. Add employee");
+    char option = Convert.ToChar(Console.ReadLine());
+
+    switch (option)
+    {
+        case '1':
+
+            break;
+        case '2':
+            company1.AddUser(e);
+
+            break;
+        case '3':
+
+            break;
+    }
+}*/
+Company company1 = new Company("Socar kbr");
+Employee e = new Employee("vugar", "sariyev", "vugar123");
+char option;
+do 
+{
+    Console.WriteLine("1. Create a company");
+    Console.WriteLine("2. Create an employee");
+    Console.WriteLine("3. Add employee");
+    Console.WriteLine("4. Get one employee");
+    Console.WriteLine("5. To update employee data");
+    Console.WriteLine("6. To see all users");
+    Console.WriteLine("7. To remove user\n");
+    option = Convert.ToChar(Console.ReadLine());
+
+    switch (option)
+    {
+        case '1':
+            company1 = new Company("Socar kbr");
+            break;
+        case '2':
+            e = new Employee("vugar", "sariyev", "vugar123");
+            break;
+        case '3':
+            company1.AddUser(e);
+            break;
+        case '4':
+            company1.GetUser(e.Username);
+            break;
+        case '5':
+            company1.UpdateUser(e.Username);
+            break;
+        case '6':
+            company1.GetAllUser();
+            break;
+        case '7':
+            company1.RemoveUser(e.Username);
+            break;
+        default:
+            Console.WriteLine("Choose from options");
+            break;
+    }
+} while (option != 'q');
